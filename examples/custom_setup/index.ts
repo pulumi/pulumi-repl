01795f9c7c0020556f08d653223d9e517ba51adc
evanboyle <evan@pulumi.com>
2021-05-08 21:10:00 -0700

    repl example with custom setup

---
+++ examples/custom_setup/index.ts
@@ -0,0 +1,28 @@
+import * as prepl from "pulumi-repl";
+// import a custom pulumi provider SDK.
+import * as random from "@pulumi/random";
+
+const repl = new prepl.PulumiRepl({
+    stack: "dev",
+    project: "custom_setup",
+    ephemeral: true, // delete stack resources on repl exit
+    config: {
+        "hello": { value: "world"} 
+        /**
+         * try entering the following in the repl:
+         * > var config = new pulumi.Config();
+         * > console.log(config.get("hello"));
+         */
+    }
+});
+
+// make the random SDK accessible to the repl at runtime
+repl.addContext("random", random);
+
+// access the automation api stack to install provider plugins
+repl.stack.then(s => {
+    // install the 'random' resource provider plugin
+    s.workspace.installPlugin("random", "v4.0.0").then(()=>{
+        repl.start().catch(err => console.error(err));
+    })
+});
