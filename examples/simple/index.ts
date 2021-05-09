6f26dc2f4c291dd4cdfd64b9e508e58a16f98b44
evanboyle <evan@pulumi.com>
2021-05-08 21:09:12 -0700

    cleanup

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -3,10 +3,9 @@ import * as prepl from "pulumi-repl";
 const repl = new prepl.PulumiRepl({
     stack: "dev",
     project: "foo",
-    ephemeral: true,
     config: {
         "aws:region": { value: "us-west-2" }
-    }
+    },
 });
 
 repl.start().catch(err => console.error(err));
